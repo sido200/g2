cd2876323a2f92c7013b9473b6c382fd872f8463
Nada Hanad <jn_hanad@esi.dz>
2022-10-22 17:12:33 +0100

    cart add

--- main.js
+++ main.js
@@ -1,5 +1,36 @@
 const modal = document.querySelector(".modal");
 const cartButton = document.querySelector(".cart-button");
+let cartItems = [];
 cartButton.addEventListener("click", () => {
   modal.classList.toggle("active");
+});
+
+const addToCartButtons = document.querySelectorAll(".button");
+addToCartButtons.forEach((item) => {
+  item.addEventListener("click", (event) => {
+    let cartItem = {
+      title: event.target.parentNode.parentNode.querySelector("h3").innerText,
+      price: Number(
+        event.target.parentNode.querySelector("h4").innerText.slice(0, -1)
+      ),
+      quantity: 1,
+    };
+    if (cartItems.find((e) => e.title === cartItem.title)) {
+      alert("Item already in your cart!");
+    } else {
+      cartItems.push(cartItem);
+      modal.innerHTML += `  <div class="cart-item">
+        <h3>${cartItem.title}</h3>
+        <p>${cartItem.price}$</p>
+        <div class="quantity">
+          <i class="fa-solid fa-minus"></i>
+          <p>${cartItem.quantity} </p>
+          <i class="fa-solid fa-plus"></i>
+        </div>
+        <h2>${cartItem.price * cartItem.quantity}$</h2>
+      </div>`;
+    }
+
+    console.log(cartItem);
+  });
 });
